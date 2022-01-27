31bb1709ab1a7234f09a2f15913d41474eebba02
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-01-27 11:57:34 -0600

    Added JS3

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log("Ciao bella me piacere parlare in italiano");
+
+// VARIABLES
+const happyFace = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣"];
+const scaryFace = ["👻", "💀", "👽", "👾", "🎃", "👹", "👺", "🤡"];
+let bothArr;
+
+//Self-Invoking Function
+const selfInvokFunc = (function (arrOne, arrTwo) {
+  let bothArr = [...arrOne, ...arrTwo];
+  return bothArr;
+})(happyFace, scaryFace);
+
+console.log(selfInvokFunc);
+
+// Linking to HTML
+const arrayOne = document.getElementById("arrayOne");
+const arrayTwo = document.getElementById("arrayTwo");
+const bothArray = document.getElementById("bothArray");
+
+arrayOne.innerText = happyFace;
+arrayTwo.innerText = scaryFace;
+bothArray.innerText = selfInvokFunc;
